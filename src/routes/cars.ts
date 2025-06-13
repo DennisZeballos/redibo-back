c7807b4332bb77712772689eff680039492f4931
Dennis Zeballos <deniszeballos15@gmail.com>
2025-06-13 15:52:37 -0400

    fetch upstream a la repo original

--- src/routes/cars.ts
+++ src/routes/cars.ts
@@ -10,6 +10,7 @@ const router = express.Router();
 const db = new PrismaClient();
 
 
+
 interface AutoRequestBody { // corregir en el otro luagar donde se usa esto
   idPropietario: number;        // idUsuario del propietario
   idUbicacion: number;          // idUbicacion de la ubicación (relación)
